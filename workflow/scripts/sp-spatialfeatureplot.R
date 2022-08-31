a5472cfd95a85969db4f34edb326fb6c5dfbf073
Sinan Uğur Umu <sinanugur@gmail.com>
2022-08-31 14:17:33 +0200

    fixes

--- workflow/scripts/sp-spatialfeatureplot.R
+++ workflow/scripts/sp-spatialfeatureplot.R
@@ -36,7 +36,7 @@ function_image_fixer(Spatial_Data,opt$sampleid) -> Spatial_Data
 
 
 plot1 <- VlnPlot(Spatial_Data, features = "nCount_Spatial", pt.size = 0.1) + NoLegend()
-plot2 <- SpatialFeaturePlot(Spatial_Data, features = "nCount_Spatial",images=paste0("image"),pt.size.factor=1.1) + theme(legend.position = "right") + scale_fill_continuous(type="viridis")
+plot2 <- SpatialFeaturePlot(Spatial_Data, features = "nCount_Spatial",images=paste0("image"),pt.size.factor=1.1) + theme(legend.position = "right",legend.title = element_blank())
 wrap_plots(plot1, plot2) -> wp
 
 
